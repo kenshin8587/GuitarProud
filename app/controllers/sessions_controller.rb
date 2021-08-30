482750963aac1cd01872592fbc2ea8be34a5725f
USER <user@USERnoMacBook-Air.local>
2021-08-30 17:48:07 +0900

    user login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,36 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    email = params[:session][:email].downcase
+    password = params[:session][:password]
+    
+    if login(email, password)
+      flash[:success] = 'ようこそ！'
+      redirect_to @user
+    else
+      flash.now[:error] = 'ログインできませんでした'
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:succuss] = 'ログアウトしました'
+    redirect_to root_url
+  end
+
+  private
+  
+  def login(email, password)
+    @user = User.find_by(email: email)
+    
+    if @user && @user.authenticate(password)
+      session[:user_id] = @user.id
+      true
+    else
+      false
+    end
+  end
+end
